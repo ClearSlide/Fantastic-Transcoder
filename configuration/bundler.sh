1fc18780e27e004c12469f6c12d689b28c2cd48c
Gabe Abinante <gabinante@clearslide.com>
2017-08-16 17:01:29 -0700

    get segment workin'

--- configuration/bundler.sh
+++ configuration/bundler.sh
@@ -8,10 +8,17 @@
   virtualenv env
   source $FTPATH/env/bin/activate
   pip install ffmpy
-
+  pip install boto3
   ## Install ffmpeg
   yum update
-  yum -y install autoconf automake build-essential git-core libass-devel.x86_64 libgpac-dev libsdl1.2-dev libtheora-dev libtool libx264-dev libvdpau-dev libvorbis-dev libx11-dev libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev libmp3lame-dev nasm gcc yasm && true
+  yum -y install autoconf automake build-essential git-core libgpac-dev libsdl1.2-dev libtheora-dev libtool libx264-dev libvdpau-dev libvorbis-dev libx11-dev libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev libmp3lame-dev nasm gcc yasm && true
+  #advanced subtitle support coming sometime
+  #wget https://downloads.sourceforge.net/freetype/freetype-2.8.tar.bz2
+  #wget https://github.com/libass/libass/releases/download/0.13.7/libass-0.13.7.tar.xz
+  #wget https://www.fribidi.org/download/fribidi-0.19.7.tar.bz2
+  #tar -xvf libass-0.13.7.tar.xz
+  #tar -xvf freetype-2.8.tar.bz2
+  #tar -xvf fribidi-0.19.7.tar.bz2
 
   mkdir $FTPATH/ffmpeg_sources
 
@@ -47,27 +54,27 @@
               --enable-libmp3lame --enable-libx264 --enable-nonfree
   make
   make install
-  cp ffmpeg $FTPATH
+  cp -r $FTPATH/ffmpeg_sources/ffmpeg $FTPATH
   make distclean
   hash -r
   ffmpeg 2>&1 | head -n1
 
   # Bundle lambda packages
-  rm $FTPATH/poll.zip
+  rm -f $FTPATH/poll.zip
   cd $FTPATH;zip -9 poll.zip poll.py
   cd $FTPATH/env/lib/python2.7/site-packages/;zip -r9 $FTPATH/poll.zip ./*
 
-  rm $FTPATH/segment.zip
+  rm -f $FTPATH/segment.zip
   cd $FTPATH;zip -9 segment.zip segment.py
   cd $FTPATH;zip -r9 segment.zip ffmpeg
   cd $FTPATH/env/lib/python2.7/site-packages;zip -r9 $FTPATH/segment.zip ./*
 
-  rm $FTPATH/convert.zip
+  rm -f $FTPATH/convert.zip
   cd $FTPATH;zip -9 convert.zip convert.py
   cd $FTPATH;zip -r9 convert.zip ffmpeg
   cd $FTPATH/env/lib/python2.7/site-packages;zip -r9 $FTPATH/convert.zip ./*
 
-  rm $FTPATH/concat.zip
+  rm -f $FTPATH/concat.zip
   cd $FTPATH;zip -9 concat.zip concat.py
   cd $FTPATH;zip -r9 concat.zip ffmpeg
   cd $FTPATH/env/lib/python2.7/site-packages;zip -r9 $FTPATH/concat.zip ./*
--- segment.py
+++ segment.py
@@ -1,10 +1,9 @@
-import boto3, ffmpy, os
+import boto3, ffmpy, os, json
 
 s3 = boto3.resource('s3')
 dynamo = boto3.resource('dynamodb')
 table = dynamo.Table('FT_SegmentState')
 sqs = boto3.resource('sqs')
-#statusqueue = sqs.Queue(sqs.get_queue_by_name(QueueName='FT_status_queue'))
 
 # Triggered by write to FT_VideoConversions
 def lambda_handler(event, context):
@@ -18,101 +17,128 @@ def lambda_handler(event, context):
         Filename, Extension = os.path.splitext(Row['Filename']['S'])
         Path = Row['Path']['S']
         StatusQueueMessageID = Row['QueueMessageID']['S']
+        RequestedFormats = Row['RequestedFormats']['S']
     except KeyError:
         print "DynamoDB records are incomplete!"
     else:
-        try:
-            os.makedirs('/tmp/{}'.format(ConversionID))
-        except Exception as e:
-            print "Directory already exists? Lambda is reusing a container."
-        if Path is 'NULL':
+        if Path == 'NULL':
             S3Path = '{}{}'.format(Filename, Extension)
-        elif Path is not 'NULL':
+        elif Path != 'NULL':
             S3Path = '{}{}{}'.format(Path, Filename, Extension)
-        LocalPath = '/tmp/{}/{}{}'.format(ConversionID, Filename, Extension)
+        LocalPath = '/tmp/{}{}'.format(Filename, Extension)
 
         print 'Bucket/ConversionID is {}, {}'.format(Bucket, ConversionID)
         print 'StatusQueueMessageID is {}'.format(StatusQueueMessageID)
 
+        '''
+        statusqueue.send_message(
+            MessageBody='Downloading source from S3...',
+            MessageAttributes={
+                'ConversionID': {
+                    'StringValue': ConversionID,
+                    'DataType': 'String'
+                }
+            }
+        )'''
         try:
-            '''
-            statusqueue.send_message(
-                MessageBody='Downloading source from S3...',
-                MessageAttributes={
-                    'ConversionID': {
-                        'StringValue': ConversionID,
-                        'DataType': 'String'
-                    }
-                }
-            )'''
-
-            s3.Bucket(Bucket).download_file(S3Path, LocalPath)
-
-            '''
-            statusqueue.send_message(
-                MessageBody='Segmenting video...',
-                MessageAttributes={
-                    'ConversionID': {
-                        'StringValue': ConversionID,
-                        'DataType': 'String'
-                    }
-                }
-            )'''
-            # Segment video with ffmpeg
-            segment(LocalPath)
-
-            '''
-            statusqueue.send_message(
-                MessageBody='Uploading segments to S3',
-                MessageAttributes={
-                    'ConversionID': {
-                        'StringValue': ConversionID,
-                        'DataType': 'String'
-                    }
-                }
-            )'''
-            # Upload each segment to S3
-            FilePath, Extension = os.path.splitext(LocalPath)
-            print 'Uploading segments and audio to s3...'
-            for filename in os.listdir('/tmp/{}/'.format(ConversionID)):
-                s3.Bucket(Bucket).upload_file('/tmp/{}/{}'.format(ConversionID, filename), '{}{}'.format(Path, filename))
-                if filename.endswith('mp3'):
-                    SegmentID = '-1'
-                else:
-                    segments = os.path.splitext(filename)[0].split('SEGMENT')
-                    SegmentID = segments[len(segments) - 1]
-
-                # Write to FT_SegmentState
-                response = table.put_item(
-                                Item = {
-                                    'Bucket': Bucket,
-                                    'ConversionID': ConversionID,
-                                    'Completed': 0,
-                                    'Filename': filename,
-                                    'Path': Path,
-                                    'QueueMessageID': QueueMessageID,
-                                    'RequestedFormats': RequestedFormats,
-                                    'SegmentID': SegmentID,
-                                }
-                            )
-                print('PutItem succeeded: {}'.format(json.dumps(response, indent=4)))
-
+            print "Cleaning working directory"
+            for f in os.listdir('/tmp/'):
+                os.remove(f)
         except Exception as e:
-            raise Exception('Failure during segmentation for file {} in bucket {}!'.format(Filename, Bucket))
+            raise Exception('Failed to clean temp dir')
+        try:
+            print "Downloading original file..."
+            s3.Bucket(Bucket).download_file(S3Path, LocalPath)
+        except Exception as e:
+            raise Exception('Failure downloading file from s3: {} to local: {} Exception: {}'.format(S3Path, LocalPath, e))
+        '''
+        statusqueue.send_message(
+            MessageBody='Segmenting video...',
+            MessageAttributes={
+                'ConversionID': {
+                    'StringValue': ConversionID,
+                    'DataType': 'String'
+                }
+            }
+        )'''
+        # Segment video with ffmpeg
+        segment(LocalPath)
+        print "Removing original file..."
+        os.remove(LocalPath)
+        '''
+        statusqueue.send_message(
+            MessageBody='Uploading segments to S3',
+            MessageAttributes={
+                'ConversionID': {
+                    'StringValue': ConversionID,
+                    'DataType': 'String'
+                }
+            }
+        )'''
+        # Upload each segment to S3
+        print 'Uploading segments and audio to s3...'
+        for filename in os.listdir('/tmp/'):
+            attempts = 0
+            while attempts < 3:
+                try:
+                    print 'uploading {} to s3'.format(filename)
+                    if Path != 'NULL':
+                        s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}{}'.format(Path, filename))
+                        break
+                    elif Path == 'NULL':
+                        s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}'.format(filename))
+                        break
+                except Exception as UploadError:
+                    attempts += 1
+                    raise Exception('Failure reuploading segment /tmp/{}, attempt {}'.format(filename, attempts))
+
+            # Write to FT_SegmentState
+            if filename.endswith('mp3'):
+                SegmentID = '-1'
+            else:
+                segments = os.path.splitext(filename)[0].split('SEGMENT')
+                SegmentID = segments[len(segments) - 1]
+                print "preparing dynamo statement for SegmentID {}".format(SegmentID)
+            writeattempts = 0
+            while writeattempts < 3:
+                try:
+                    print 'Writing information to DynamoDB...'
+                    print "Bucket: {} ConversionID: {} Filename: {} Path: {} StatusQueueMessageID: {} RequestedFormats: {} SegmentID: {}".format(Bucket, ConversionID, filename, Path, StatusQueueMessageID, RequestedFormats, SegmentID)
+                    response = table.put_item(
+                                    Item = {
+                                        'Bucket': Bucket,
+                                        'ConversionID': ConversionID,
+                                        'Completed': 0,
+                                        'Filename': filename,
+                                        'Path': Path,
+                                        'QueueMessageID': StatusQueueMessageID,
+                                        'RequestedFormats': RequestedFormats,
+                                        'SegmentID': SegmentID,
+                                    }
+                                )
+                    formattedresponse = json.dumps(response, indent=4)
+                    print('PutItem succeeded: {}'.format(formattedresponse))
+                    break
+                except Exception as DynamoError:
+                    raise Exception('Failure writing data to dynamodb for segment {}, attempt {} Exception: {}'.format(SegmentID, attempts, DynamoError))
+                    writeattempts += 1
 
 # ffmpy invocation that SEGMENTs the video into chunks
 def segment(path):
     if path is not None:
-        FilePath, Extension = os.path.splitext(path)
-        f = ffmpy.FFmpeg(
-                executable='./ffmpeg/ffmpeg',
-                inputs={path : None},
-                outputs={'{}.mp3'.format(FilePath): '-c copy'})
-        ff = ffmpy.FFmpeg(
-                executable='./ffmpeg/ffmpeg',
-                inputs={path : None},
-                outputs={'{}SEGMENT%d{}'.format(FilePath, Extension): '-acodec copy -c:a libfdk_aac -f segment -vcodec copy -reset_timestamps 1 -map 0'})
-        f.run()
-        ff.run()
-        print "Segmenting done~"
-        os.remove(path)
+        try:
+            print "Segmenting video file..."
+            FilePath, Extension = os.path.splitext(path)
+            f = ffmpy.FFmpeg(
+                    executable='./ffmpeg/ffmpeg',
+                    inputs={path : None},
+                    outputs={'{}.mp3'.format(FilePath): '-c:a mp3'})
+            ff = ffmpy.FFmpeg(
+                    executable='./ffmpeg/ffmpeg',
+                    inputs={path : None},
+                    outputs={'{}SEGMENT%d{}'.format(FilePath, Extension): '-acodec copy -f segment -vcodec copy -reset_timestamps 1 -map 0'})
+            f.run()
+            ff.run()
+            print "Segmentation complete"
+        except Exception as ffmpegerror:
+            raise Exception('Failure during segmentation of video file')
