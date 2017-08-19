0adba7d15fb65c5bba037d891dc5f65a967b6bcb
Jonathan Tan <wawadragaan@gmail.com>
2017-08-19 10:36:04 -0700

    touchups here and there

--- segment.py
+++ segment.py
@@ -20,97 +20,96 @@ def lambda_handler(event, context):
         RequestedFormats = Row['RequestedFormats']['M']
     except KeyError:
         print "DynamoDB records are incomplete! {}".format(KeyError)
-    else:
-        if Path == 'NULL':
-            S3Path = '{}{}'.format(Filename, Extension)
-        elif Path != 'NULL':
-            S3Path = '{}{}{}'.format(Path, Filename, Extension)
-        LocalPath = '/tmp/{}{}'.format(Filename, Extension)
+    if Path == 'NULL':
+        S3Path = '{}{}'.format(Filename, Extension)
+    elif Path != 'NULL':
+        S3Path = '{}{}{}'.format(Path, Filename, Extension)
+    LocalPath = '/tmp/{}{}'.format(Filename, Extension)
 
-        print 'Bucket/ConversionID is {}, {}'.format(Bucket, ConversionID)
-        print 'StatusQueueMessageID is {}'.format(StatusQueueMessageID)
+    print 'Bucket/ConversionID is {}, {}'.format(Bucket, ConversionID)
+    print 'StatusQueueMessageID is {}'.format(StatusQueueMessageID)
 
-        '''
-        statusqueue.send_message(
-            MessageBody='Downloading source from S3...',
-            MessageAttributes={
-                'ConversionID': {
-                    'StringValue': ConversionID,
-                    'DataType': 'String'
-                }
+    '''
+    statusqueue.send_message(
+        MessageBody='Downloading source from S3...',
+        MessageAttributes={
+            'ConversionID': {
+                'StringValue': ConversionID,
+                'DataType': 'String'
             }
-        )'''
-        try:
-            print "Cleaning working directory"
-            for f in os.listdir('/tmp/'):
-                os.remove(f)
-        except Exception as e:
-            raise Exception('Failed to clean temp dir')
-        try:
-            print "Downloading original file..."
-            s3.Bucket(Bucket).download_file(S3Path, LocalPath)
-        except Exception as e:
-            raise Exception('Failure downloading file from s3: {} to local: {} Exception: {}'.format(S3Path, LocalPath, e))
-        '''
-        statusqueue.send_message(
-            MessageBody='Segmenting video...',
-            MessageAttributes={
-                'ConversionID': {
-                    'StringValue': ConversionID,
-                    'DataType': 'String'
-                }
+        }
+    )'''
+    try:
+        print "Cleaning working directory"
+        for f in os.listdir('/tmp/'):
+            os.remove('/tmp/{}'.format(f))
+    except Exception as e:
+        raise Exception('Failed to clean temp dir')
+    try:
+        print "Downloading original file..."
+        s3.Bucket(Bucket).download_file(S3Path, LocalPath)
+    except Exception as e:
+        raise Exception('Failure downloading file from s3: {} to local: {} Exception: {}'.format(S3Path, LocalPath, e))
+    '''
+    statusqueue.send_message(
+        MessageBody='Segmenting video...',
+        MessageAttributes={
+            'ConversionID': {
+                'StringValue': ConversionID,
+                'DataType': 'String'
             }
-        )'''
-        # Segment video with ffmpeg
-        segment(LocalPath)
-        print "Removing original file..."
-        os.remove(LocalPath)
-        '''
-        statusqueue.send_message(
-            MessageBody='Uploading segments to S3',
-            MessageAttributes={
-                'ConversionID': {
-                    'StringValue': ConversionID,
-                    'DataType': 'String'
-                }
+        }
+    )'''
+    # Segment video with ffmpeg
+    segment(LocalPath)
+    print "Removing original file..."
+    os.remove(LocalPath)
+    '''
+    statusqueue.send_message(
+        MessageBody='Uploading segments to S3',
+        MessageAttributes={
+            'ConversionID': {
+                'StringValue': ConversionID,
+                'DataType': 'String'
             }
-        )'''
-        # Upload each segment to S3
-        print 'Uploading segments and audio to s3...'
-        for filename in os.listdir('/tmp/'):
-            attempts = 0
-            while attempts < 3:
-                try:
-                    print 'uploading {} to s3'.format(filename)
-                    if Path != 'NULL':
-                        s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}{}'.format(Path, filename))
-                        break
-                    elif Path == 'NULL':
-                        s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}'.format(filename))
-                        break
-                except Exception as UploadError:
-                    attempts += 1
-                    raise Exception('Failure reuploading segment /tmp/{}, attempt {}'.format(filename, attempts))
+        }
+    )'''
+    # Upload each segment to S3
+    print 'Uploading segments and audio to s3...'
+    for filename in os.listdir('/tmp/'):
+        attempts = 0
+        while attempts < 3:
+            try:
+                print 'uploading {} to s3'.format(filename)
+                if Path != 'NULL':
+                    s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}{}'.format(Path, filename))
+                    break
+                elif Path == 'NULL':
+                    s3.Bucket(Bucket).upload_file('/tmp/{}'.format(filename), '{}'.format(filename))
+                    break
+            except Exception as UploadError:
+                attempts += 1
+                raise Exception('Failure reuploading segment /tmp/{}, attempt {}'.format(filename, attempts))
 
-            # Write to FT_SegmentState
-            if filename.endswith('mp3'):
-                SegmentID = '-1'
-            else:
-                segments = os.path.splitext(filename)[0].split('SEGMENT')
-                SegmentID = segments[len(segments) - 1] + "-" + ConversionID
-                print "preparing dynamo statement for SegmentID {}".format(SegmentID)
-            writeattempts = 0
-            while writeattempts < 3:
-                try:
-                    print 'Writing information to DynamoDB...'
-                    print "Bucket: {} ConversionID: {} Filename: {} Path: {} StatusQueueMessageID: {} RequestedFormats: {} SegmentID: {}".format(Bucket, ConversionID, filename, Path, StatusQueueMessageID, RequestedFormats, SegmentID)
-                    response = table.put_item(
-                                    Item = {
-                                        'Bucket': Bucket,
-                                        'ConversionID': ConversionID,
-                                        'Completed': 0,
-                                        'Filename': filename,
-                                        'Path': Path,
+        # Write to FT_SegmentState
+        if filename.endswith('mp3'):
+            SegmentID = '-1'
+        else:
+            segments = os.path.splitext(filename)[0].split('SEGMENT')
+            SegmentID = segments[len(segments) - 1] + "-" + ConversionID
+            print "preparing dynamo statement for SegmentID {}".format(SegmentID)
+        writeattempts = 0
+        while writeattempts < 3:
+            try:
+                print 'Writing information to DynamoDB...'
+                print "Bucket: {} ConversionID: {} Filename: {} Path: {} StatusQueueMessageID: {} RequestedFormats: {} SegmentID: {}".format(Bucket, ConversionID, filename, Path, StatusQueueMessageID, RequestedFormats, SegmentID)
+                response = table.put_item(
+                                Item = {
+                                    'Bucket': Bucket,
+                                    'ConversionID': ConversionID,
+                                    'Completed': 0,
+                                    'Filename': filename,
+                                    'Path': Path,
                                         'QueueMessageID': StatusQueueMessageID,
                                         'RequestedFormats': RequestedFormats,
                                         'SegmentID': SegmentID,
